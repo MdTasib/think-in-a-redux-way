eb8381fe90fe1fd45243f53256af8ed3ec0f145b
MdTasib <ohidulalam442@gmail.com>
2022-10-27 09:05:59 +0600

    redux devtools extension

--- redux-fundamental-with-react/src/redux/store.js
+++ redux-fundamental-with-react/src/redux/store.js
@@ -1,8 +1,12 @@
 import rootReducer from "./rootReducer";
 import myLogger from "./middlewares/myLogger";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 const { createStore, applyMiddleware } = require("redux");
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(logger, myLogger))
+);
 
 export default store;
