e09d6b140212484b11fafa271c08f3169a969a72
MdTasib <ohidulalam442@gmail.com>
2022-10-27 11:42:55 +0600

    total price and total items

--- redux-fundamental-with-react/src/components/shoppingCart/Cart.js
+++ redux-fundamental-with-react/src/components/shoppingCart/Cart.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 
 const Cart = () => {
 	const cartItems = useSelector(state => state.shoppingCart.cart);
+	const [totalPrice, setTotalPrice] = useState(0);
+	const [totalItems, setTotalItems] = useState(0);
+
+	useEffect(() => {
+		let items = 0;
+		let price = 0;
+
+		cartItems.forEach(item => {
+			items += item.qty;
+			price += item.price * item.qty;
+		});
+
+		setTotalPrice(price);
+		setTotalItems(items);
+	}, [cartItems, totalPrice, setTotalPrice, totalItems, setTotalItems]);
 
 	return (
 		<div class='col-span-12 sm:col-span-12 md:col-span-5 lg:col-span-4 xxl:col-span-4'>
@@ -18,7 +33,7 @@ const Cart = () => {
 				<div class='flex justify-center items-center text-center'>
 					<div class='text-xl font-semibold'>
 						<p>Total Item</p>
-						<p class='text-5xl'>0</p>
+						<p class='text-5xl'>{totalItems}</p>
 					</div>
 				</div>
 			</div>
@@ -26,7 +41,7 @@ const Cart = () => {
 				<div class='flex justify-center items-center text-center'>
 					<div class='text-xl font-semibold'>
 						<p>Total Price</p>
-						<p class='text-5xl'>0</p>
+						<p class='text-5xl'>{totalPrice}</p>
 					</div>
 				</div>
 			</div>
