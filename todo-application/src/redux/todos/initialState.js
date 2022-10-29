66f19c6354b1a1d312d28644cb9c4d40586cb495
MdTasib <ohidulalam442@gmail.com>
2022-10-29 22:31:02 +0600

    todoReducer

---
+++ todo-application/src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+const initialState = [
+	{
+		id: 1,
+		text: "Learn React Js",
+		completed: true,
+	},
+	{
+		id: 2,
+		text: "Learn Reuxt With Sumit",
+		completed: false,
+		color: "red",
+	},
+];
+
+export default initialState;
