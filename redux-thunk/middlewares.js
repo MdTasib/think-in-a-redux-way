f68107c502c39999a61f905fe720e9d442e26f5f
MdTasib <ohidulalam442@gmail.com>
2022-11-06 11:47:18 +0600

    redux thunk

---
+++ redux-thunk/middlewares.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+const delayActionMiddleware = store => next => action => {
+	if (action.type === "todos/todoAdded") {
+		console.log("i'm delaying you!");
+
+		setTimeout(() => {
+			next(action);
+		}, 2000);
+
+		return;
+	}
+
+	return next(action);
+};
+
+const fetchAsyncMiddleware = store => next => async action => {
+	if (typeof action === "function") {
+		return action(store.dispatch, store.getState);
+	}
+
+	return next(action);
+};
+
+module.exports = { delayActionMiddleware, fetchAsyncMiddleware };
