08164857ca5d9f396a22cc78557cc5f365e85567
MdTasib <ohidulalam442@gmail.com>
2022-11-10 17:23:19 +0600

    part 2

--- redux-toolkit-without-react/rtk/app/store.js
+++ redux-toolkit-without-react/rtk/app/store.js
@@ -1,6 +1,7 @@
 const confiqureStore = require("@reduxjs/toolkit").configureStore;
 const counterReducer = require("../features/counter/counterSlice");
 const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounter");
+const postsReducer = require("../features/posts/postSlice");
 const { createLogger } = require("redux-logger");
 
 const logger = createLogger();
@@ -9,6 +10,7 @@ const store = confiqureStore({
 	reducer: {
 		counter: counterReducer,
 		dynamicCounter: dynamicCounterReducer,
+		posts: postsReducer,
 	},
 	middleware: getDefaultMiddlewares => {
 		return getDefaultMiddlewares().concat(logger);
