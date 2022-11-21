3b0f4799e94e3dfa6bb2b8b42d39b31cb2a7e4fe
MdTasib <ohidulalam442@gmail.com>
2022-11-21 19:23:27 +0600

    filter slice done

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
 
 export default function Tag({ title }) {
+	const dispatch = useDispatch();
+	const { tags } = useSelector(state => state.filter);
+
+	const isSelected = tags.includes(title) ? true : false;
+	const styles = isSelected
+		? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+		: "bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer";
+
+	function handleSelect() {
+		if (isSelected) {
+			dispatch(tagRemoved(title));
+		} else {
+			dispatch(tagSelected(title));
+		}
+	}
+
 	return (
-		<div className='bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer'>
+		<div className={styles} onClick={handleSelect}>
 			{title}
 		</div>
 	);
-}
-
-{
-	/* <div
-    className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
->
-    redux
-</div> */
 }
